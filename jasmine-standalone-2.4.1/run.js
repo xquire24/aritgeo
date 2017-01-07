fc47e2e386a06eb6708453d0165ea7555500b386
charles <xquireworld@gmail.com>
2017-01-07 11:44:06 +0000

    testing for conditions

--- jasmine-standalone-2.4.1/run.js
+++ jasmine-standalone-2.4.1/run.js
@@ -11,3 +11,12 @@ var aritGeo = function(list){
           for(var i=1; i<list.length; i++){
                test1 = list[i] - list[i-1];
                test2 = list[i] / list[i-1];
+     //Testing whether the elements conforms to the conditions of the ar and geo progressions        
+               if(test1 != diff1){
+                   is_arr = false;
+               }
+               
+               if(test2 != diff2){
+                   is_geo = false;
+               }
+           }
