ab55e5f89b5ed378ae78d08878968b65c98da166
charles <xquireworld@gmail.com>
2017-01-07 11:42:34 +0000

    setting conditions 2

--- jasmine-standalone-2.4.1/run.js
+++ jasmine-standalone-2.4.1/run.js
@@ -3,3 +3,11 @@ var aritGeo = function(list){
        if(list.length > 0){
            var is_arr = true;
            var is_geo = true;
+
+//setting conditions for checking the arithmetic and geometric progressions of elements of the list
+           diff1 = list[1] - list[0];
+           diff2 = list[1] / list[0];
+          
+          for(var i=1; i<list.length; i++){
+               test1 = list[i] - list[i-1];
+               test2 = list[i] / list[i-1];
